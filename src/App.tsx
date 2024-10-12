1ec0bf12333878c55996c86135cb52b00dee381f
escapeWu <escape@shancw.net>
2024-10-12 10:20:51 +0800

    add loading state

--- src/App.tsx
+++ src/App.tsx
@@ -3,24 +3,31 @@ import './App.css'
 import { saveAs } from 'file-saver'
 
 function App() {
+  const worker = new Worker('worker.js');
+  
   const [files, setFiles] = useState<File[]>([])
+
+  const [loading, setLoading] = useState(false)
 
   const handleFileChange = (event: any) => {
     setFiles(Array.from(event.target.files))
   }
 
   const handleSave = () => {
+
+    setLoading(true)
     
-    const worker = new Worker('worker.js');
     worker.postMessage({ files });
 
     worker.onmessage = function (e) {
       const blobContent = e.data;
       saveAs(blobContent, "xx.zip");
+      setLoading(false)
     };
 
     worker.onerror = function (error) {
       console.error('Error from worker:', error);
+      setLoading(false)
     };
   }
 
@@ -28,7 +35,7 @@ function App() {
     <>
       <div>
         <input type="file" multiple onChange={handleFileChange} />
-        {files.length ? <button onClick={handleSave}>Compress</button> : null}
+        {files.length ? <button onClick={handleSave}> {loading ? '处理中' : '压缩'}</button> : null}
       </div>
     </>
   )
